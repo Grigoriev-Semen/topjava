b60c94f63d3ef211518aca067cfe90e0ef01bb12
Grigoriev_Semen <byblik.sem@yandex.ru>
2022-11-05 15:40:38 +0300

    Add methods getWithMeals and getWithUser, make test

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.javawebinar.topjava.model.Meal;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -26,5 +26,8 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
              SELECT m FROM Meal m 
                 WHERE m.user.id=:userId AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC
             """)
-    List<Meal> getBetweenHalfOpen(@Param("startDateTime")LocalDateTime startDateTime,@Param("endDateTime") LocalDateTime endDateTime,@Param("userId") int userId);
+    List<Meal> getBetweenHalfOpen(@Param("startDateTime") LocalDateTime startDateTime, @Param("endDateTime") LocalDateTime endDateTime, @Param("userId") int userId);
+
+    @Query("SELECT m FROM Meal m JOIN FETCH m.user WHERE m.id =:id AND m.user.id =:user_id")
+    Meal getWithUser(@Param("id") int id, @Param("user_id") int userId);
 }
